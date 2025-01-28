78cd838e4461090bddd8ffd2d66785b0d9d10115
Roslik Ivan <iroslik.dev@dent-it.ru>
2025-01-28 16:37:59 +0300

    [dev] build and nuget push first working state

---
+++ app-build/dotnet-builder/_publish-package-scripts/nuget-push.sh
@@ -0,0 +1,9 @@
+echo "Configuration: $1"
+echo "Server address: $2"
+echo "Server port: $3"
+echo "api key: $4"
+cd Bin/$1
+for f in *.nupkg
+do
+dotnet nuget push -s http://$2:$3/v3/index.json -k $4 $f
+done
